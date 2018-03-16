27934ca123d0c6e9379e0638f595d519e4fb6b26
Yezhel Marcial Ruiz <yezhelmr@gmail.com>
2018-03-16 17:32:14 -0600

    Creación de la interfaz y de algunos objetos para las operaciones

---
+++ calculadora/js/codigo.js
@@ -0,0 +1,48 @@
+/*=============================================
+OBJETOS CON LAS PROPIEDADES DE LA CALCULADORA
+=============================================*/
+
+var p = {
+	teclas: document.querySelectorAll("#calculadora ul li"),
+	accion: null
+}
+
+
+/*=============================================
+OBJETOS DCON LOS MÉTODOS DE LA CALCULADORA
+=============================================*/
+
+var m = {
+	inicio: function(){
+
+		for(var i=0; i< p.teclas.length; i++){
+			
+			p.teclas[i].addEventListener("click",m.oprimirTecla)
+		}
+	},
+
+	oprimirTecla: function(tecla){
+		p.accion =tecla.target.getAttribute("class");
+		m.calculadora(p.accion);
+
+	},
+
+	calculadora: function(accion){
+		switch(accion){
+			case "numero":
+				console.log("numero");
+			break;
+			case "signo":
+				console.log("signo");
+			break;
+			case "decimal":
+				console.log("decimal");
+			break;
+			case "igual":
+				console.log("igual");
+			break;
+		}
+	}
+}
+
+m.inicio();
